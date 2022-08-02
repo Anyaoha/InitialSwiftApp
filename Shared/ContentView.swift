4610b2263b25c64be5b056a60935248a04728ce4
anyaoha <roczyuche@yahoo.com>
2022-08-01 19:27:27 -0700

    update  on init

--- Shared/ContentView.swift
+++ Shared/ContentView.swift
@@ -9,7 +9,7 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("Hello, world!")
+        Text("Hello, all welcome to my world!")
             .padding()
     }
 }
