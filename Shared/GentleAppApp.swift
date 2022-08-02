a975760c05a2f91db66de1bab9fc2df6a982e8f5
anyaoha <roczyuche@yahoo.com>
2022-08-01 19:20:33 -0700

    Initial Commit

---
+++ Shared/GentleAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  GentleAppApp.swift
+//  Shared
+//
+//  Created by uche on 8/1/22.
+//
+
+import SwiftUI
+
+@main
+struct GentleAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
